dd1990611e4d0c48a3517eff6d92a885d451d91d
mbret <bret.maxime@gmail.com>
2014-03-11 09:15:34 +0100

    Merge origin/master
    Conflicts:
    	manifest.mf

---
+++ src/main/java/com/mycompany/jenseits_von_theben/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.jenseits_von_theben;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
