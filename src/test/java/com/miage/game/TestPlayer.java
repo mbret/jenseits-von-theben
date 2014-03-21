8e69b1032e37d82ce621e767fc80207c250f087d
mbret <bret.maxime@gmail.com>
2014-03-21 16:38:10 +0100

    refrech project

---
+++ src/test/java/com/miage/game/TestPlayer.java
@@ -0,0 +1,44 @@
+package com.miage.game;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+
+public class TestPlayer {
+	
+	private Player player;
+	PlayerKnowledges playerKnowledges;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+		player = new Player("Test");
+		
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	
+	
+	
+
+}
